8d103e1a37cda41f95170222909b1fc41a26c98f
Palakjethi <palakjethi123@gmail.com>
2026-07-14 09:29:21 +0530

    Add missing tests folder

---
+++ tests/test_main.py
@@ -0,0 +1,22 @@
+﻿from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_read_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello from inside a Docker container!"}
+
+
+def test_health_check():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
+
+
+def test_add_numbers():
+    response = client.get("/add/2/3")
+    assert response.status_code == 200
+    assert response.json()["sum"] == 5
